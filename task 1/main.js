2d72567f433f4d28a4d18594134bf89520f035c6
LegjobbCenter <kalmanbarnabas4@gamil.com>
2022-05-04 17:18:18 +0200

    main

---
+++ task 1/main.js
@@ -0,0 +1,37 @@
+
+
+//--------------------------------this function alerts 1 word-----------------------------------
+
+
+/*function randomWord() {
+    
+}
+
+let num1 =  document.getElementById(num1);
+let num2 = document.getElementById(num2);
+
+
+
+//------------------------this function sums 2 numbers----------------------------------------- 
+
+function sum () {
+    let value = num1 + num2;
+    alert(value)
+}
+sum()
+*/
+
+
+
+
+//-------------------------this function returns the square of a number-------------------------
+
+
+// This function works well 
+
+function square() {
+    let number = prompt("enter a number")
+    let sqNum = number * number
+    alert(sqNum)
+}
+square()
